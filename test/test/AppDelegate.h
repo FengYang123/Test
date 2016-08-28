f5e37305243e07b69e9bfcae511820f1f832b54c
“fy123” <1270271401@qq.com>
2016-08-28 14:25:45 +0800

    测试
    测试新建项目

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by fengyang on 16/8/28.
+//  Copyright © 2016年 fengyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
